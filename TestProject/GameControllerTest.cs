ed1550c9ab9902e477520769b90fec6252f7cb22
lukaradulovicdocusketch <luka.radulovic.011@gmail.com>
2024-10-25 18:28:34 +0200

    Added test classes

---
+++ TestProject/GameControllerTest.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLSAppTest
+{
+
+    [TestClass]
+    public class GameControllerTest 
+    {
+        private GameController _controller;
+        private Mock<IGameMaster> _mockGameMaster;
+        private Player _player;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _mockGameMaster = new Mock<IGameMaster>();
+            _player = new Player();
+            _controller = new GameController(_mockGameMaster.Object, _player);
+        }
+
+
+        [TestMethod]
+        public void GetChoices_WithMockService_ReturnChoices()
+        {
+            //Arrange
+            var result = _controller.Choices() as OkObjectResult;
+
+
+            //Assert
+            Assert.AreEqual(200, result.StatusCode);
+        }
+
+        [TestMethod]
+        public void GetChoice_WithMockService_ReturnChoice()
+        {
+            //Arrange
+            var result = _controller.Choice() as OkObjectResult;
+
+
+            //Assert
+            Assert.AreEqual(200, result.StatusCode);
+        }
+
+        [TestMethod]
+        public void Play_ValidChoice_ReturnSuccessCase()
+        {
+            // Arrange 
+            var validShape = (int)Shape.Rock;
+            var choice = new PlayerChoiceModel { Player = validShape };
+
+            // Act
+            var result = _controller.Play(choice) as OkObjectResult;
+
+            // Assert
+            Assert.AreEqual(200, result.StatusCode);
+  
+        }
+
+        [TestMethod]
+        public void Play_InvalidChoice_ReturnFailureCase()
+        {
+            // Arrange
+            var invalidShape = 999; 
+            var choice = new PlayerChoiceModel { Player = invalidShape };
+
+            // Act
+            var result = _controller.Play(choice) as BadRequestObjectResult;
+
+            // Assert
+            Assert.AreEqual("Invalid shape provided.", result.Value);
+        }
+
+        [TestMethod]
+        public void Play_NullChoice_ReturnFailureCase()
+        {
+            // Arrange
+            var choice = new PlayerChoiceModel { Player = null };
+
+            // Act
+            var result = _controller.Play(choice) as BadRequestObjectResult;
+
+            // Assert
+            Assert.AreEqual(400, result.StatusCode);
+        }
+    }
+}
---
+++ TestProject/GameMasterTest.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLSAppTest
+{
+    [TestClass]
+    public class GameMasterTest
+    {
+        private Mock<IGameLogic> _mockGameLogic;
+        private Player _player;
+        private GameMaster _gameMaster;
+
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _mockGameLogic = new Mock<IGameLogic>();
+            _player = new Player();
+            _gameMaster = new GameMaster(_mockGameLogic.Object);
+        }
+
+        [TestMethod]
+        public void Test_Creating_Computer_PlayerFailure()
+        {
+            // Arrange
+            Player cpuPlayer = _gameMaster.InstanciateComputerPlayer();
+
+            // Assert
+            Assert.IsNotNull(cpuPlayer);
+        }
+
+        [TestMethod]
+        public void Test_Creating_Computer_PlayerSuccess()
+        {
+            // Arrange
+            Player cpuPlayer = _gameMaster.InstanciateComputerPlayer();
+
+            // Assert
+            Assert.IsTrue(Enum.IsDefined(typeof(Shape), cpuPlayer.Shape));
+        }
+
+        // TODO: Correct tests
+        [TestMethod]
+        public void PlayGame_Success()
+        {
+            // Arrange
+            Player player = new Player();
+            player.Shape = Shape.Rock;
+
+            // Act
+            var result = _gameMaster.PlayGame(player);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(MatchResult));
+        }
+
+        [TestMethod]
+        public void PlayGame_Failure()
+        {
+            // Arrange
+            Player invalidPlayer = new Player { Shape = (Shape)99 }; 
+
+            // Act
+            _gameMaster.PlayGame(invalidPlayer);
+
+            // Assert 
+            var exception = Assert.ThrowsException<ArgumentException>(() =>
+            {
+                _gameMaster.PlayGame(invalidPlayer);
+            });
+
+            // Optional: Check the exception message if necessary
+            Assert.AreEqual("Invalid player input: shape must be a defined enum value.", exception.Message);
+        }
+    }
+}
