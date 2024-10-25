5117b8b618668625c0770051bd1ff189870075d2
lukaradulovicdocusketch <luka.radulovic.011@gmail.com>
2024-10-25 17:40:58 +0200

    readed errorview

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -1,9 +1,7 @@
-namespace RPSLSTest.Models
-{
+
     public class ErrorViewModel
     {
         public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
-}
