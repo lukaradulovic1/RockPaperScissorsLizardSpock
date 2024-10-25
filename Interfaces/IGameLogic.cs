26bc8e9f7b4469e875d9b3c34ef874c157d9ccd6
lukaradulovicdocusketch <luka.radulovic.011@gmail.com>
2024-10-25 14:14:12 +0200

    Added IGameLogic interface to provide logic for the game but be interchangeable depending on the game rules

---
+++ Interfaces/IGameLogic.cs
@@ -0,0 +1,9 @@
+﻿
+public interface IGameLogic
+    {
+    public Dictionary<int, List<string>> InstanciateGameOutcomes();
+    public MatchResult DetermineWinner(Player player, Player cpuPlayer, Dictionary<int, List<string>> rule);
+    public List<Choice> Choices();
+    public Choice Choice();
+
+    }
