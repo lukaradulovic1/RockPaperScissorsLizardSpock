6536ca7ceb1392446936bc11d0f22bbe6201db9a
lukaradulovicdocusketch <luka.radulovic.011@gmail.com>
2024-10-25 14:21:30 +0200

    Added MatchResult to be returned as the API JSON response

---
+++ GameLogicFolder/MatchResult.cs
@@ -0,0 +1,14 @@
+﻿public class MatchResult
+{
+    public int Player { get; set; }
+    public int Computer { get; set; }
+    public string Results { get; set; }
+
+    public MatchResult(int playerShape, int computerShape, string result)
+    {
+        Player = playerShape;
+        Computer = computerShape;
+        Results = result;
+    }
+
+}
