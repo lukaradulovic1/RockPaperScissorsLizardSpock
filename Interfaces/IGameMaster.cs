7515d6027d2c1aa97e5b895b4bf28a91f500037f
lukaradulovicdocusketch <luka.radulovic.011@gmail.com>
2024-10-25 14:14:44 +0200

    Added IGameMaster to control the flow of the game but also be interchangeable depending on the rules or player count

---
+++ Interfaces/IGameMaster.cs
@@ -0,0 +1,9 @@
+﻿
+public interface IGameMaster
+{
+        public MatchResult PlayGame(Player player);
+        public Player InstanciateComputerPlayer();
+        public List<Choice> Choices();
+        public Choice GetRandomChoice();
+
+    }
