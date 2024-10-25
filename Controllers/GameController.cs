c87a5a2f1406efb6efe065ad24c047c9b165424a
lukaradulovicdocusketch <luka.radulovic.011@gmail.com>
2024-10-25 14:27:53 +0200

    Added constructor

--- Controllers/GameController.cs
+++ Controllers/GameController.cs
@@ -6,6 +6,13 @@ using System.Numerics;
 [ApiController]
 public class GameController : Controller
 {
-   
+    private readonly IGameMaster _gameMaster;
+    private readonly Player _player;
+
+    public GameController(IGameMaster gameMaster, Player player)
+    {
+        this._gameMaster = gameMaster;
+        this._player = player;
+    }
 }
 
